16a2f2be0ac87940836e87a8b648b46e8fd54db9
alex <epicYoshi64@gmail.com>
2017-05-19 19:48:33 -0600

    Added Empty Scripts

---
+++ AI Madness/Assets/AI Scripts/WeaponManger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
