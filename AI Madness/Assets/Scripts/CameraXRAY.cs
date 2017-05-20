f4df39811cdc421d0ba448fce7a015b54db46297
matt <matthew_fyfe@hotmail.com>
2017-05-20 16:15:44 -0600

    Camera Magic

--- AI Madness/Assets/Scripts/CameraXRAY.cs
+++ AI Madness/Assets/Scripts/CameraXRAY.cs
@@ -12,12 +12,43 @@ public class CameraXRAY : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		RaycastHit[] hits;
-		hits = Physics.RaycastAll(transform.position, transform.forward, 100.0F);
+		RaycastHit[] hitsBack;
+		hits = Physics.RaycastAll(transform.position, transform.forward, 10.0F);
+		hitsBack = Physics.RaycastAll(transform.position, transform.forward*-1, 1000.0F);
 
 		for (int i = 0; i < hits.Length; i++) {
 			RaycastHit hit = hits [i];
-
 			if (hit.transform.gameObject.tag == "XRay") {
+
+				Debug.Log ("XRay" + hit.transform.name);
+
+				Renderer rend = hit.transform.GetComponent<Renderer> ();
+
+				if (rend) {
+					// Change the material of all hit colliders
+					// to use a transparent shader.
+					rend.material.shader = Shader.Find ("Transparent/Diffuse");
+					Color tempColor = rend.material.color;
+					tempColor.a = 0.3F;
+					rend.material.color = tempColor;
+
+					//set tag to GHOST
+					hit.transform.gameObject.tag = "GHOST";
+				}
+				//invoke to reset transparancy later
+				if(hit.transform.gameObject.tag == "GHOST")
+				{
+					StartCoroutine (ResetTransparancy (hit.transform.gameObject));
+				}
+			}
+		}
+
+		for (int i = 0; i < hitsBack.Length; i++) {
+			RaycastHit hit = hitsBack [i];
+			if (hit.transform.gameObject.tag == "XRay") {
+
+				Debug.Log ("XRay" + hit.transform.name);
+
 				Renderer rend = hit.transform.GetComponent<Renderer> ();
 
 				if (rend) {
